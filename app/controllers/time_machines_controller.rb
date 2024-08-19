597a503e2b733e6fea77fb5bd6688d05bd8cf7ef
Zainan Ali <zainanzaher09@gmail.com>
2024-08-19 16:33:05 +0100

    Fixed bugs

--- app/controllers/time_machines_controller.rb
+++ app/controllers/time_machines_controller.rb
@@ -1,2 +1,17 @@
 class TimeMachinesController < ApplicationController
+
+  def new
+    @machine = TimeMachine.new
+  end
+
+  def create
+    @user = current_user
+    raise
+  end
+
+  private
+
+  def timemachine_params
+    params.require(:time_machine).permit(:name, :size, :speed, :comfort)
+  end
 end
--- db/migrate/20240819124819_add_user_to_bookings.rb
+++
@@ -1,6 +0,0 @@
-class AddUserToBookings < ActiveRecord::Migration[7.1]
-  def change
-    add_column :bookings, :buyer_id, :bigint
-    add_foreign_key :bookings, :users, column: :buyer_id
-  end
-end
