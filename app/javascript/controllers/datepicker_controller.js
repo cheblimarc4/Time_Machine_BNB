fb6fe93354946c9adb204bb8f87883a023e66549
Zainan Ali <zainanzaher09@gmail.com>
2024-08-22 15:24:29 +0100

    Added stuff

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -2,7 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
 
 export default class extends Controller {
+  static values = {
+    lastbooking: Object
+  }
   connect() {
-    flatpickr(this.element)
+    console.log(this.lastbookingValue);
+    flatpickr(this.element, {
+        minDate: this.lastbookingValue.end_date || "today"
+    })
   }
 }
